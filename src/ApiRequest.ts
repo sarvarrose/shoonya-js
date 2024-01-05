bb04c1a6a4ea64b95d9304222b331e53af44ab70
Sarvar Rose <47109217+sarvarrose@users.noreply.github.com>
2024-01-05 13:59:09 +0530

    feat: create-client-class (#2)
    * feat: add Client with login and logout methods
    
    * chore: change build target to es6
    
    * chore: enable logging for api request and response
    
    * feat: allow authentication using dpin
    
    * feat: allow authentication using dpinadd method to set dpin
    
    * feat: add methods for gettings positions limits and holdings
    
    * feat: add new apis

---
+++ src/ApiRequest.ts
@@ -0,0 +1,77 @@
+import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios'
+import routes from './routes.json'
+
+/**
+ * @description Class to make API requests to the Shoonya API
+ * @property {string} [accessToken] - The access token to use for the API requests
+ * @property {boolean} [debug] - Whether to log debug information to the console
+ */
+export default class ApiRequest {
+  static baseURL = 'https://api.shoonya.com/NorenWClientTP'
+  accessToken: string
+  debug: boolean
+  requestInstance: any
+
+  constructor(accessToken?: string, debug?: boolean) {
+    this.accessToken = accessToken ?? ''
+    this.debug = debug ?? false
+
+    this.requestInstance = axios.create({ baseURL: ApiRequest.baseURL })
+
+    this.requestInstance.interceptors.request.use(
+      (request: AxiosRequestConfig) => {
+        if (this.debug) {
+          // console.info('\nRequest: ', {
+          //   url: request.url,
+          //   method: request.method,
+          //   data: request.data
+          // })
+        }
+
+        return request
+      }
+    )
+
+    this.requestInstance.interceptors.response.use(
+      (response: AxiosResponse) => {
+        if (this.debug) {
+          console.info('Response: ', {
+            url: response.config.url,
+            method: response.config.method,
+            data: response.data
+          })
+        }
+
+        if ('stat' in response.data && response.data.stat === 'Not_Ok') {
+          throw new Error(
+            response.data?.emsg ?? response.data?.message ?? 'Unknown error'
+          )
+        }
+
+        return response.data
+      },
+      async (error: any) => {
+        if (this.debug) {
+          console.error(error)
+        }
+
+        return await Promise.reject(error)
+      }
+    )
+  }
+
+  async post(
+    route: keyof typeof routes,
+    params?: Record<string, any>
+  ): Promise<AxiosResponse<any, any>> {
+    // compose payload
+    let payload = 'jData=' + JSON.stringify(params)
+    if (this.accessToken) {
+      payload = payload + `&jKey=${this.accessToken}`
+    }
+
+    const response = await this.requestInstance.post(routes[route], payload)
+
+    return response
+  }
+}
---
+++ src/Client.ts
@@ -0,0 +1,210 @@
+import ApiRequest from './ApiRequest'
+import sha256 from './utils'
+
+type ProductName = 'C' | 'M' | 'I' | 'B' | 'H'
+type Segment = 'CM' | 'FO' | 'FX'
+
+export default class Client {
+  readonly #requestInstance: any
+  /** authenticated user's ID */
+  userId: string
+  /** accountId's value will be same as userId */
+  accountId: string
+  #accessToken: string
+  /** debug mode */
+  debug: boolean
+
+  constructor({
+    userId,
+    accessToken,
+    debug
+  }: {
+    userId: string
+    accessToken?: string
+    debug?: boolean
+  }) {
+    this.userId = userId
+    this.accountId = userId
+    this.#accessToken = accessToken ?? ''
+    this.debug = debug ?? false
+
+    // TODO: check if there is any api call that can be made witout authentication
+    // if not, we do not need to pass accessToken to ApiRequest here
+    this.#requestInstance = new ApiRequest(this.#accessToken, this.debug)
+  }
+
+  #setAccessToken(accessToken: string): void {
+    this.#accessToken = accessToken
+    this.#requestInstance.accessToken = accessToken
+  }
+
+  #setUserId(userId: string): void {
+    this.userId = userId
+    this.accountId = userId
+  }
+
+  /**
+   * @description Login to the Shoonya API
+   * **NOTE:** If pin is passed, authentication will be done using pin instead of factor2
+   * @param {string} password - Password
+   * @param {string} vendorCode - Vendor code
+   * @param {string} apiKey - API key
+   * @param {string} [factor2] - OTP or TOTP
+   * @param {string} [pin] - PIN
+   * @param {string} [imei=api] - IMEI of the mobile / MAC address of the desktop
+   */
+  async login(
+    password: string,
+    vendorCode: string,
+    apiKey: string,
+    factor2?: string,
+    pin?: string,
+    imei?: string
+  ): Promise<any> {
+    if (!factor2 && !pin) {
+      throw new Error('Either factor2 or pin is required')
+    }
+
+    const url = !factor2 && pin ? 'pinAuth' : 'login'
+    const payload = {
+      source: 'API',
+      apkversion: 'js:1.0.0',
+      uid: this.userId,
+      pwd: sha256(password),
+      vc: vendorCode,
+      appkey: sha256(`${this.userId}|${apiKey}`),
+      imei: imei ?? 'api',
+      factor2: factor2 || undefined,
+      dpin: !factor2 && pin ? sha256(pin) : undefined
+    }
+
+    // TODO: set types
+    const data = await this.#requestInstance.post(url, payload)
+
+    if (!data.susertoken) {
+      throw new Error(data.emsg ?? 'Login attempt failed')
+    }
+
+    this.#setAccessToken(data.susertoken)
+    this.#setUserId(data.actid)
+
+    if (factor2 && pin) {
+      await this.setPin(pin, imei)
+    }
+
+    return data
+  }
+
+  /**
+   * @description Logout
+   */
+  async logout(): Promise<any> {
+    const payload = {
+      uid: this.userId
+    }
+
+    const data = await this.#requestInstance.post('logout', payload)
+
+    return data
+  }
+
+  /**
+   * @description Set account PIN to allow authenticating using PIN instead of 2FA
+   * @param {string} pin - PIN
+   * @param {string} [imei=api] - IMEI of the mobile / MAC address of the desktop
+   */
+  async setPin(pin: string, imei?: string): Promise<any> {
+    const payload = {
+      source: 'API',
+      uid: this.userId,
+      dpin: pin,
+      imei: imei ?? 'api'
+    }
+
+    const data = await this.#requestInstance.post('setPin', payload)
+
+    return data
+  }
+
+  /**
+   * @description Get user's holdings
+   * @param {string} [productName=C] - Product name
+   */
+  async getHoldings(productName?: ProductName): Promise<any> {
+    const payload = {
+      uid: this.userId,
+      actid: this.accountId,
+      prd: productName ?? 'C'
+    }
+
+    const data = await this.#requestInstance.post('holdings', payload)
+
+    return data
+  }
+
+  /**
+   * @description Get user's limits
+   * @param {string} [productName] - Product name
+   * @param {string} [segment] - Segment
+   * @param {string} [exchange] - Exchange
+   */
+  async getLimits(
+    productName?: ProductName,
+    segment?: Segment,
+    exchange?: string
+  ): Promise<any> {
+    const payload = {
+      uid: this.userId,
+      actid: this.accountId,
+      prd: productName || undefined,
+      seg: segment || undefined,
+      exch: exchange || undefined
+    }
+
+    const data = await this.#requestInstance.post('limits', payload)
+
+    return data
+  }
+
+  /**
+   * @description Get user's positions
+   */
+  async getPositions(): Promise<any> {
+    const payload = {
+      uid: this.userId,
+      actid: this.accountId
+    }
+
+    const data = await this.#requestInstance.post('positions', payload)
+
+    return data
+  }
+
+  /**
+   * @description Get user's max payout amount
+   */
+  async getMaxPayoutAmount(): Promise<any> {
+    const payload = {
+      uid: this.userId,
+      actid: this.accountId
+    }
+
+    const data = await this.#requestInstance.post('maxPayoutAmount', payload)
+
+    return data
+  }
+
+  async getUnSettledTradingDate(): Promise<any> {
+    const payload = {
+      uid: this.userId,
+      actid: this.accountId
+    }
+
+    const data = await this.#requestInstance.post(
+      'unSettledTradingDate',
+      payload
+    )
+
+    return data
+  }
+}
